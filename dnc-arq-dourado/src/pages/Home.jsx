4b25d7645c140c94d363b511cead98b377248bf5
eduardo dourado <165962217+douradosD@users.noreply.github.com>
2025-03-10 23:05:11 -0300

    atualizar

--- dnc-arq-dourado/src/pages/Home.jsx
+++ dnc-arq-dourado/src/pages/Home.jsx
@@ -3,16 +3,15 @@ import Button from "../components/Button/Button"
 import Header from "../components/Header/Header"
 import Hero from "../components/Hero/Hero"
 import Footer from "../components/Footer/Footer"
+import ProjectsList from "../components/ProjectsList/ProjectsList"
 
 function Home() {
     return (
         <>
             <Header />
-            <h1>Home</h1>
-            <p>Subtitle</p>
-            <Button buttonStyle="secondary" arrow>olá!</Button>
             <div className="container">
                 <Hero/>
+                <ProjectsList/>
             </div>
             <Footer />
 
