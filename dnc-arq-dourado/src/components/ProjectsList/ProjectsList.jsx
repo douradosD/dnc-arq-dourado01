162afdb469ed874375f1cf246598a51d46417d3a
douradosD <165962217+douradosD@users.noreply.github.com>
2025-03-25 22:39:21 -0300

    atualizar

--- dnc-arq-dourado/src/components/ProjectsList/ProjectsList.jsx
+++ dnc-arq-dourado/src/components/ProjectsList/ProjectsList.jsx
@@ -1,10 +1,28 @@
+import { useState, useEffect } from 'react'
 import './ProjectsList.css'
 
 //ASSETS
 import LikedFilled from '../../assets/like_filled.svg'
 import Like from '../../assets/like.svg'
 
-function ProjectsList(props) {
+//UTILS
+import { getApiData } from '../../services/apiServices'
+
+function ProjectsList() {
+    const [projects, setProjects] = useState()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const projectsResponse = await getApiData('projects')
+                setProjects(projectsResponse)
+            } catch {
+                setProjects([])
+            }
+        }
+        fetchData()
+    }, [])
+
     return (
      <div className="projects-section">
         <div className="projects-hero">
@@ -12,54 +30,19 @@ function ProjectsList(props) {
            <p>It is a long established fact that a reader will be distracted by the of readable content of page  lookings at its layouts  points.</p>
         </div>
         <div className="projects-grid">
-            <div className="projects-card d-flex jc-center al-center fd-column">
-                <div className="thumb tertiary-background"></div>
-                <h3>joão silva</h3>
-                <p>BH, Brasil</p>
-                <img src={LikedFilled} height="20px"/>
-            </div>
-            <div className="projects-card d-flex jc-center al-center fd-column">
-                <div className="thumb tertiary-background"></div>
-                <h3>joão silva</h3>
-                <p>BH, Brasil</p>
-                <img src={LikedFilled} height="20px"/>
-            </div>
-            <div className="projects-card d-flex jc-center al-center fd-column">
-                <div className="thumb tertiary-background"></div>
-                <h3>joão silva</h3>
-                <p>BH, Brasil</p>
-                <img src={Like} height="20px"/>
-            </div>
-            <div className="projects-card d-flex jc-center al-center fd-column">
-                <div className="thumb tertiary-background"></div>
-                <h3>joão silva</h3>
-                <p>BH, Brasil</p>
-                <img src={LikedFilled} height="20px"/>
-            </div>
-            <div className="projects-card d-flex jc-center al-center fd-column">
-                <div className="thumb tertiary-background"></div>
-                <h3>joão silva</h3>
-                <p>BH, Brasil</p>
-                <img src={LikedFilled} height="20px"/>
-            </div>
-            <div className="projects-card d-flex jc-center al-center fd-column">
-                <div className="thumb tertiary-background"></div>
-                <h3>joão silva</h3>
-                <p>BH, Brasil</p>
-                <img src={LikedFilled} height="20px"/>
-            </div>
-            <div className="projects-card d-flex jc-center al-center fd-column">
-                <div className="thumb tertiary-background"></div>
-                <h3>joão silva</h3>
-                <p>BH, Brasil</p>
-                <img src={LikedFilled} height="20px"/>
-            </div>
-            <div className="projects-card d-flex jc-center al-center fd-column">
-                <div className="thumb tertiary-background"></div>
-                <h3>joão silva</h3>
-                <p>BH, Brasil</p>
-                <img src={LikedFilled} height="20px"/>
-            </div>
+            {
+                projects.map((projects) => (
+                    <div className="projects-card d-flex jc-center al-center fd-column" key={projects.id}>
+                           <div 
+                                className="thumb tertiary-background">
+                                
+                            </div>
+                            <h3>joão silva</h3>
+                            <p>BH, Brasil</p>
+                            <img src={LikedFilled} height="20px"/>
+                    </div>
+                ))
+            }
         </div>
      </div>
     )
